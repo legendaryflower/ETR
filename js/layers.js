171085b4403e2da335d2297e234d2f2d50677351
SlimeAdventure1 <94472938+SlimeAdventure1@users.noreply.github.com>
2023-06-15 09:59:06 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -16,7 +16,7 @@ addLayer("p", {
     gainMult() {
         let mult = new Decimal(1)
         if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
-        if (hasUpgrade("p", 22)) mult = mult.times(upgradeEffect("p", 22));
+  
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
