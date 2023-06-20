661b33c6ea598af6332e6e81b8f5085250f0682d
SlimeAdventure1 <94472938+SlimeAdventure1@users.noreply.github.com>
2023-06-20 13:56:21 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
 addLayer("p", {
-    name: "machintruc", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "machintrc", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
